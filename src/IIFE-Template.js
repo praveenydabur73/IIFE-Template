9a6a640d94a73e417d1e2b05ebc919e02fb287a4
praveenyadavadl <praveen.yadav@adviserlogic.com>
2019-01-05 00:42:04 +0530

    commit

---
+++ src/IIFE-Template.js
@@ -0,0 +1,11 @@
+/*!
+ * Immediately Invoked Function Expression Boilerplate
+ * (c) 2018 Praveen Yadav, MIT License
+ */
+;(function (window, document, undefined) {
+
+    'use strict';
+
+    // Code goes here...
+
+})(window, document);
